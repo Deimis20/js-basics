476be82c3989eefb5b14a52448750bc6aacc97e9
Deimis20 <144778222+Deimis20@users.noreply.github.com>
2023-11-05 17:30:12 +0200

    Done

--- js/js.js
+++ js/js.js
@@ -1,85 +1,86 @@
-// const knygos = {
-//   grozine: [
-//     {
-//       ISBN: "GR1236K",
-//       leidimoMetai: 2023,
-//       pavadinimas: "Mano mintys debesyse",
-//       pslSkaicius: 300,
-//     },
-//     {
-//       ISBN: "GR1286K",
-//       leidimoMetai: 2015,
-//       pavadinimas: "Mano mintys juroje",
-//       pslSkaicius: 256,
-//     },
-//     {
-//       ISBN: "GR1436K",
-//       leidimoMetai: 2016,
-//       pavadinimas: "Virdulys orkaiteje",
-//       pslSkaicius: 321,
-//     },
-//     {
-//       ISBN: "GR1536K",
-//       leidimoMetai: 1987,
-//       pavadinimas: "Namuose kate",
-//       pslSkaicius: 251,
-//     },
-//   ],
-//   // -----------------------------------------------
-//   teisine: [
-//     {
-//       ISBN: "GR1277K",
-//       leidimoMetai: 2005,
-//       pavadinimas: "Nuosprendis",
-//       pslSkaicius: 425,
-//     },
-//     {
-//       ISBN: "GR1278K",
-//       leidimoMetai: 2007,
-//       pavadinimas: "Nuosprendis tesinys",
-//       pslSkaicius: 422,
-//     },
-//     {
-//       ISBN: "GR1587K",
-//       leidimoMetai: 2023,
-//       pavadinimas: "Vadovo pergale",
-//       pslSkaicius: 425,
-//     },
-//     {
-//       ISBN: "GR1727K",
-//       leidimoMetai: 2002,
-//       pavadinimas: "Rimtuma",
-//       pslSkaicius: 325,
-//     },
-//   ],
-//   // ---------------------------------------------
-//   religine: [
-//     {
-//       ISBN: "GR1114K",
-//       leidimoMetai: 2006,
-//       pavadinimas: "Religija",
-//       pslSkaicius: 865,
-//     },
-//     {
-//       ISBN: "GR1217K",
-//       leidimoMetai: 2005,
-//       pavadinimas: "Religines vertybes",
-//       pslSkaicius: 555,
-//     },
-//     {
-//       ISBN: "GR1481K",
-//       leidimoMetai: 2023,
-//       pavadinimas: "Tvarumas",
-//       pslSkaicius: 445,
-//     },
-//     {
-//       ISBN: "GR1685K",
-//       leidimoMetai: 2018,
-//       pavadinimas: "Karti tiesa",
-//       pslSkaicius: 315,
-//     },
-//   ],
-// };
+const knygos = {
+  grozine: [
+    {
+      ISBN: "GR1236K",
+      leidimoMetai: 2023,
+      pavadinimas: "Mano mintys debesyse",
+      pslSkaicius: 300,
+    },
+    {
+      ISBN: "GR1286K",
+      leidimoMetai: 2015,
+      pavadinimas: "Mano mintys juroje",
+      pslSkaicius: 256,
+    },
+    {
+      ISBN: "GR1436K",
+      leidimoMetai: 2016,
+      pavadinimas: "Virdulys orkaiteje",
+      pslSkaicius: 321,
+    },
+    {
+      ISBN: "GR1536K",
+      leidimoMetai: 1987,
+      pavadinimas: "Namuose kate",
+      pslSkaicius: 251,
+    },
+  ],
+
+  // ---------------------------------------------
+  religine: [
+    {
+      ISBN: "GR1114K",
+      leidimoMetai: 2006,
+      pavadinimas: "Religija",
+      pslSkaicius: 865,
+    },
+    {
+      ISBN: "GR1217K",
+      leidimoMetai: 2005,
+      pavadinimas: "Religines vertybes",
+      pslSkaicius: 555,
+    },
+    {
+      ISBN: "GR1481K",
+      leidimoMetai: 2023,
+      pavadinimas: "Tvarumas",
+      pslSkaicius: 445,
+    },
+    {
+      ISBN: "GR1685K",
+      leidimoMetai: 2018,
+      pavadinimas: "Karti tiesa",
+      pslSkaicius: 315,
+    },
+  ],
+  // -----------------------------------------------
+  teisine: [
+    {
+      ISBN: "GR1277K",
+      leidimoMetai: 2005,
+      pavadinimas: "Nuosprendis",
+      pslSkaicius: 425,
+    },
+    {
+      ISBN: "GR1278K",
+      leidimoMetai: 2007,
+      pavadinimas: "Nuosprendis tesinys",
+      pslSkaicius: 422,
+    },
+    {
+      ISBN: "GR1587K",
+      leidimoMetai: 2023,
+      pavadinimas: "Vadovo pergale",
+      pslSkaicius: 425,
+    },
+    {
+      ISBN: "GR1727K",
+      leidimoMetai: 2002,
+      pavadinimas: "Rimtuma",
+      pslSkaicius: 325,
+    },
+  ],
+};
 // console.log(knygos);
 
 // for (let kategorija in knygos) {
@@ -97,6 +98,43 @@
 //     console.log("--------------");
 //   }
 // }
+
+const grozineButton = document.getElementById("grozineButton");
+const religineButton = document.getElementById("religineButton");
+const teisineButton = document.getElementById("teisineButton");
+const grozineContent = document.getElementById("grozineContent");
+const reiligineContent = document.getElementById("religineContent");
+const teisineContent = document.getElementById("teisineContent");
+let numgrozine = document.getElementById("grozineButton").attributes.length;
+let numreligine = document.getElementById("religineButton").attributes.length;
+let numteisine = document.getElementById("teisineButton").attributes.length;
+grozineButton.addEventListener("click", () =>
+  displayBooks("grozine", innerHTML = numgrozine, grozineContent)
+);
+religineButton.addEventListener("click", () =>
+  displayBooks("religine", (innerHTML = numreligine), reiligineContent)
+);
+teisineButton.addEventListener("click", () =>
+  displayBooks("teisine", innerHTML = numteisine, teisineContent)
+);
+
+
+
+function displayBooks(category, content) {
+  content.innerHTML = "";
+  const kategorija = knygos[category];
+  for (const knyga of kategorija) {
+    const knygaInfo = document.createElement("div");
+    knygaInfo.innerHTML = `
+            ISBN: ${knyga.ISBN}<br>
+            Leidimo metai: ${knyga.leidimoMetai}${
+      knyga.leidimoMetai == 2023 ? " (nauja knyga)" : ""}<br>
+            Pavadinimas: ${knyga.pavadinimas}<br>
+            Puslapių skaičius: ${knyga.pslSkaicius}<br>
+            --------------------------<br>`;
+    content.appendChild(knygaInfo);
+  }
+}
 
 //--------------------------------------------------------------------------
 
@@ -118,13 +156,13 @@
 //  * Naudodami JavaScript sukurkite HTML lentele, kuria sudaro 3 eilutes ir 10 stuplepiu.
 //  */
 
-var table = document.createElement('table'), tr, td, row, cell;
-for (row = 0; row < 3; row++) {
-    tr = document.createElement('tr');
-    for (cell = 0; cell < 10; cell++) {
-        td = document.createElement('td');
-        tr.appendChild(td);
-    }
-    table.appendChild(tr);
-}
-document.querySelector('body').appendChild(table)
+// var table = document.createElement('table'), tr, td, row, cell;
+// for (row = 0; row < 3; row++) {
+//     tr = document.createElement('tr');
+//     for (cell = 0; cell < 10; cell++) {
+//         td = document.createElement('td');
+//         tr.appendChild(td);
+//     }
+//     table.appendChild(tr);
+// }
+// document.querySelector('body').appendChild(table)
